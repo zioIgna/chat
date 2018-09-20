7ab877ac6c3edee6db322082dad5450fb59b998d
zioIgna <ignaziocarbonaro@hotmail.com>
2018-09-20 18:18:22 +0200

    Funzione chat implementata

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+/*     socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    }); */
     socket.on('disconnect', function(){
         console.log('user disconnected');
-    });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
     });
 });
 
