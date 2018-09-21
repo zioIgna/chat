c973691e6172d4c86fa33ca28d5481e3af075410
zioIgna <ignaziocarbonaro@hotmail.com>
2018-09-21 11:21:31 +0200

    Correzione: da io.emit a socket.broadcast.emit

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){   // in pratica socket identifica il client che si è connesso (?)
     console.log('a user connected');
     var mess = 'Someone just connected...';
-    // socket.broadcast.emit('new connection', mess);
-    io.emit('new connection', mess);
+    // il metodo socket.broadcast.emit è un po' come io.emit in quanto trasmette a tutti i client tranne che al client che ha emesso il segnale
+    // cfr. https://socket.io/docs/emit-cheatsheet/
+    socket.broadcast.emit('new connection', mess);
+    // io.emit('new connection', mess);    // così il messaggio verrebbe inviato anche all'emittente originale
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
@@ -18,7 +20,9 @@ io.on('connection', function(socket){   // in pratica socket identifica il clien
         console.log('message: ' + msg);
     }); */
     socket.on('disconnect', function(){
+        var mess = 'Someone just disconnected...';
         console.log('user disconnected');
+        io.emit('disconnection', mess);
     });
 });
 
